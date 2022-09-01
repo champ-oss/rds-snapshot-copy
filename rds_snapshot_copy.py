9ffc59bfb0bbf515ee6f9c9e4fb62f5b61e0ad0d
Phillip Langley <philliplangley@champmbplangley.lan>
2022-09-01 15:52:28 -0500

    update requirements to copy

--- rds_snapshot_copy.py
+++ rds_snapshot_copy.py
@@ -19,17 +19,21 @@ def delete_latest_snapshot(snapshot: str) -> None:
     logger.info("deleted latest snapshot complete")
 
 
-def copy_snapshot(source_snapshot: str, target_snapshot: str) -> None:
+def copy_snapshot(source_snapshot: str, target_snapshot: str, kms_key_id: str) -> None:
     logger.info("copy snapshot")
 
     rds.copy_db_snapshot(
         SourceDBSnapshotIdentifier=source_snapshot,
         TargetDBSnapshotIdentifier=target_snapshot,
+        KmsKeyId=kms_key_id,
     )
     logger.info("copy snapshot complete")
 
 
 def lambda_handler(event, context):
+    # env variables requirement, used to share snapshot with another account
+    kms_key_id = os.environ['KMS_KEY_ID']
+
     # get list of manual snapshots and delete
     db_snapshot_list = list()
     response = rds.describe_db_snapshots(SnapshotType='manual')
@@ -38,7 +42,7 @@ def lambda_handler(event, context):
         db_snapshot_list.append(db_snapshot_name)
     for db_snapshot in db_snapshot_list:
         print(db_snapshot)
-        #delete_latest_snapshot(db_snapshot)
+        delete_latest_snapshot(db_snapshot)
     db_snapshot_list.clear()
 
     # get a list of shared snapshots and copy
@@ -51,5 +55,5 @@ def lambda_handler(event, context):
         db_snapshot_shared_list.append(db_shared_snapshot_name)
     for db_snapshot_shared_instance in db_snapshot_shared_list:
         target_snapshot = db_snapshot_shared_instance.split(":")[6]
-        copy_snapshot(db_snapshot_shared_instance,target_snapshot)
+        copy_snapshot(db_snapshot_shared_instance,target_snapshot,kms_key_id)
     db_snapshot_shared_list.clear()
