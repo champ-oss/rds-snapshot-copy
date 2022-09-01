2026720c06b88e2cd963ad3f8db5a54c414fd08f
Phillip Langley <philliplangley@champmbplangley.lan>
2022-09-01 14:17:49 -0500

    adding rds snapshots lambda function

---
+++ README.md
@@ -0,0 +1,22 @@
+# rds-snapshot-copy
+
+Summary: Lambda python function deletes all latest manual snapshots and copies all shared rds snapshots
+
+![ci](https://github.com/conventional-changelog/standard-version/workflows/ci/badge.svg)
+[![version](https://img.shields.io/badge/version-1.x-yellow.svg)](https://semver.org)
+
+## Table of Contents
+* [General Info](#general-information)
+* [Technologies Used](#technologies-used)
+* [Project Status](#project-status)
+
+## General Information
+- Lambda python function deletes all latest manual snapshots and copies all shared rds snapshots
+
+## Technologies Used
+- python 
+- Lambda
+- github actions
+
+## Project Status
+Project is: _in_progress_
---
+++ rds_snapshot_copy.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+
+from __future__ import annotations
+
+import logging
+import boto3
+
+rds = boto3.client('rds')
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+def delete_latest_snapshot(snapshot: str) -> None:
+    logger.info("delete latest snapshot")
+    rds.delete_db_snapshot(
+        DBSnapshotIdentifier=snapshot,
+    )
+    time.sleep(5)
+    logger.info("deleted latest snapshot complete")
+
+
+def copy_snapshot(source_snapshot: str, target_snapshot: str) -> None:
+    logger.info("copy snapshot")
+
+    rds.copy_db_snapshot(
+        SourceDBSnapshotIdentifier=source_snapshot,
+        TargetDBSnapshotIdentifier=target_snapshot,
+    )
+    logger.info("copy snapshot complete")
+
+
+def lambda_handler(event, context):
+    # get list of manual snapshots and delete
+    db_snapshot_list = list()
+    response = rds.describe_db_snapshots(SnapshotType='manual')
+    for i in response['DBSnapshots']:
+        db_snapshot_name = i['DBSnapshotIdentifier']
+        db_snapshot_list.append(db_snapshot_name)
+    for db_snapshot in db_snapshot_list:
+        print(db_snapshot)
+        #delete_latest_snapshot(db_snapshot)
+    db_snapshot_list.clear()
+
+    # get a list of shared snapshots and copy
+    db_snapshot_shared_list = list()
+    response = rds.describe_db_snapshots(
+        SnapshotType='shared',
+        IncludeShared=True)
+    for i in response['DBSnapshots']:
+        db_shared_snapshot_name = i['DBSnapshotIdentifier']
+        db_snapshot_shared_list.append(db_shared_snapshot_name)
+    for db_snapshot_shared_instance in db_snapshot_shared_list:
+        target_snapshot = db_snapshot_shared_instance.split(":")[6]
+        copy_snapshot(db_snapshot_shared_instance,target_snapshot)
+    db_snapshot_shared_list.clear()
